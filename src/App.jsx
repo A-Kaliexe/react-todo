6d56ccb0dc6faa9314edf83c18d3e3593caa6be0
A-Kaliexe <145980980+A-Kaliexe@users.noreply.github.com>
2024-11-08 03:30:19 -0600

    added addtoform and todolist components

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-const todoList = [
-  { 
-    id: 1, 
-    title: 'Install Vite React'
-  },
-  { 
-    id: 2, 
-    title: 'Do Project Setup' 
-  },
-  { 
-    id: 3, 
-    title: 'Complete Assignment' 
-  },
-]
+import TodoList from './TodoList'
+import AddTodoForm from './AddTodoForm'
 
 function App() {
   return (
+  <>
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {/* render the list */}
-        {todoList.map((todo) => 
-        <li key={todo.id}>{todo.title}</li>
-      )}
-      </ul>
+        <TodoList />
+        <AddTodoForm />
     </div>
+  </>
   );
 }
 

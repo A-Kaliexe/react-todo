71d0974c5a96bb8c14543641c743ce880a6e627d
A-Kaliexe <145980980+A-Kaliexe@users.noreply.github.com>
2024-11-26 11:27:25 -0600

    Refactor Todo List to use state, controlled input, and destructure todos via props.

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
 function App() {
-  const[newTodo, setNewTodo] = useState("");
+  const[todoList, setTodoList] = useState([]);
+  function addTodo(newTodo){
+    setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
+  }
+  
    return (
   <>
     <div>
       <h1>Todo List</h1>
-        <TodoList />
-        <AddTodoForm onAddTodo={setNewTodo}/>
-        <p>{newTodo}</p>
+        <TodoList todoList={todoList}/>
+        <AddTodoForm onAddTodo={addTodo}/>
+
     </div>
   </>
   );
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,29 +1,14 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-const todoList = [
-    { 
-      id: 1, 
-      title: 'Install Vite React'
-    },
-    { 
-      id: 2, 
-      title: 'Do Project Setup' 
-    },
-    { 
-      id: 3, 
-      title: 'Complete Assignment' 
-    }
-  ]
-
-function TodoList() {
+function TodoList({todoList}) {
     return (
     <>
-        <ul>
+      <ul>
         {/* render the list */}
         {todoList.map((todo) => 
         < TodoListItem key={todo.id} todo={todo} />
-      )}
+        )}
       </ul>
     </>
     );

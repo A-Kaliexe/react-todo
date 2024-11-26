3805f209f9bbf812354bfd8f3856f84b0c74c249
A-Kaliexe <145980980+A-Kaliexe@users.noreply.github.com>
2024-11-26 11:27:25 -0600

    Refactor Todo List to use state, controlled input, and destructure todos via props.

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-function AddTodoForm(props) { 
-function handleAddTodo(e) {e.preventDefault();
-     const todoTitle = e.target.title.value;
-    console.log(todoTitle);
-    e.target.reset();
-    props.onAddTodo(todoTitle);
+function AddTodoForm({onAddTodo}) { 
+  const[todoTitle, setTodoTitle] = useState("")
+
+function handleTitleChange(e) {
+  const newTodotitle = e.target.value
+  setTodoTitle(newTodotitle);
+}
+
+function handleAddTodo(e) {
+    e.preventDefault();
+    const newTodo = { title: todoTitle, id: Date.now()}
+    onAddTodo(newTodo);
+    setTodoTitle("");
 }
 return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input type="text" id="todoTitle" name="title" />
+      <input type="text" id="todoTitle" name="title" value={todoTitle} onChange={handleTitleChange}/>
       <button type="submit">Add</button>
     </form>
   );
 }
+
 
 export default AddTodoForm;

e69c4bda6b61dfc25a6dad09aeb4112d2433cce2
A-Kaliexe <145980980+A-Kaliexe@users.noreply.github.com>
2025-01-28 21:06:18 -0600

    Added css modules for styling and formatted docs

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,32 +1,37 @@
-import { useState } from 'react';
-import InputWithLabel from './InputWithLabel';
+import { useState } from "react";
+import InputWithLabel from "./InputWithLabel";
+import style from "./AddTodoForm.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
+function AddTodoForm({ onAddTodo }) {
+  const [todoTitle, setTodoTitle] = useState("");
 
-function AddTodoForm({onAddTodo}) { 
-  const[todoTitle, setTodoTitle] = useState("")
+  function handleTitleChange(e) {
+    const newTodotitle = e.target.value;
+    setTodoTitle(newTodotitle);
+  }
 
-function handleTitleChange(e) {
-  const newTodotitle = e.target.value
-  setTodoTitle(newTodotitle);
-}
-
-function handleAddTodo(e) {
+  function handleAddTodo(e) {
     e.preventDefault();
-    const newTodo = { title: todoTitle, id: Date.now()}
+    const newTodo = { title: todoTitle, id: Date.now() };
     onAddTodo(newTodo);
     setTodoTitle("");
-}
-return (
-    <form onSubmit={handleAddTodo}>
-     <InputWithLabel 
-     label="TodoTitle" 
-     value={todoTitle}
-     onChange={handleTitleChange}
-     id="todoTitle"/>
-      <button type="submit">Add</button>
+  }
+  return (
+    <form onSubmit={handleAddTodo} className={style.addtodoform}>
+      <InputWithLabel
+        label="TodoTitle"
+        value={todoTitle}
+        onChange={handleTitleChange}
+        id="todoTitle"
+      />
+      <button type="submit" className={style.AddBtnIcon}>
+        Add
+        <FontAwesomeIcon icon={faPlus} size="sm" style={{ color: "#fafafa" }} />
+      </button>
     </form>
   );
 }
-
 
 export default AddTodoForm;
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,8 +1,21 @@
-function TodoListItem({todo, removeTodo}){
-    return(
-        <li>{todo.title}
-        <button type="button" onClick={() => removeTodo(todo.id)}>Remove</button>
-        </li>
-    )
+import style from "./TodoListItem.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+
+function TodoListItem({ todo, removeTodo }) {
+  return (
+    <li className={style.ListItem}>
+      {todo.title}
+      <button
+        type="button"
+        onClick={() => removeTodo(todo.id)}
+        className={style.RemoveBtnIcon}
+      >
+        {" "}
+        Remove
+        <FontAwesomeIcon icon={faTrashCan} style={{ color: "#fafafa" }} />
+      </button>
+    </li>
+  );
 }
-export default TodoListItem
+export default TodoListItem;

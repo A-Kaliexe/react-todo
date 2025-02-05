0bea6c239eb07409bf8d7dc27def6721786b6343
A-Kaliexe <145980980+A-Kaliexe@users.noreply.github.com>
2025-02-04 19:06:13 -0600

    assigned prop-types to jsx forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,66 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import TodoList from './TodoList'
-import AddTodoForm from './AddTodoForm'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import AddTodoForm from "./components/AddTodoForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
-  const options = {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`
-    }
-  }
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+      },
+    };
 
-  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`
+    const url = `https://api.airtable.com/v0/${
+      import.meta.env.VITE_AIRTABLE_BASE_ID
+    }/${import.meta.env.VITE_TABLE_NAME}`;
 
+    try {
+      const response = await fetch(url, options);
 
-  try {
-    const response = await fetch(url, options);
-
-    if (!response.ok) {
-      const message = `Error has ocurred:
+      if (!response.ok) {
+        const message = `Error has ocurred:
                              ${response.status}`;
-      throw new Error(message);
-    }
+        throw new Error(message);
+      }
 
-    const data = await response.json();
-    console.log(data);
-    
-    const todos = data.records.map((todo) => {
-      const newTodo =  {
+      const data = await response.json();
+      console.log(data);
+
+      const todos = data.records.map((todo) => {
+        const newTodo = {
           id: todo.id,
-          title: todo.fields.title
-        }
+          title: todo.fields.title,
+        };
 
-        return newTodo
+        return newTodo;
+      });
 
-    });
+      setTodoList(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error.message);
+      setIsLoading(false);
+    }
+  };
 
-    setTodoList(todos);
-    setIsLoading(false);
-  } catch (error) {
-    console.error(error.message);
-    setIsLoading(false);
-  }
-};
-
-//handles Airtable
-useEffect(() => {
-fetchData()
-}, []);
+  //handles Airtable
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (!isLoading) {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-      }
-    }, [isLoading, todoList]);
-    
-
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
+  }, [isLoading, todoList]);
 
   function addTodo(newTodo) {
     setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
@@ -73,25 +69,32 @@ fetchData()
   function removeTodo(id) {
     const updatedList = todoList.filter((todo) => todo.id !== id);
     setTodoList(updatedList);
+    localStorage.setItem("savedTodoList", JSON.stringify(updatedList));
   }
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={
-        <>
-          <div>
-            <h1>Todo List</h1>
-            <AddTodoForm onAddTodo={addTodo} />
-            {isLoading ? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
-          </div>
-        </>
-      } />
-      <Route path="/new" element={<h1>New Todo List</h1>} />
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div>
+                <h1>Todo List</h1>
+                <AddTodoForm onAddTodo={addTodo} />
+                {isLoading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+                )}
+              </div>
+            </>
+          }
+        />
+        <Route path="/new" element={<h1>New Todo List</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
-
+export default App;
